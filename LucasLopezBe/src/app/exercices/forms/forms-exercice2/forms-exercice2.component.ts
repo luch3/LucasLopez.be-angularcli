1f87ef69f412db24a4060053f65d524f201c7526
Lucas <lopez.lucas@hotmail.com>
2020-10-06 17:47:48 +0200

    Exercices sur les forms

--- LucasLopezBe/src/app/exercices/forms/forms-exercice2/forms-exercice2.component.ts
+++ LucasLopezBe/src/app/exercices/forms/forms-exercice2/forms-exercice2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-forms-exercice2',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsExercice2Component implements OnInit {
 
+  formNewPerson: FormGroup = new FormGroup({
+    nom: new FormControl('', Validators.required),
+    prenom: new FormControl('', Validators.required),
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  nomIsInvalid(): boolean {
+    return this.formNewPerson.controls.nom.invalid && this.formNewPerson.controls.nom.dirty && this.formNewPerson.controls.nom.touched;
+  }
+
+  prenomIsInvalid(): boolean {
+    return this.formNewPerson.controls.prenom.invalid &&
+      this.formNewPerson.controls.prenom.dirty && this.formNewPerson.controls.prenom.touched;
+  }
 }
--- LucasLopezBe/src/app/exercices/forms/forms-exercice3/forms-exercice3.component.ts
+++ LucasLopezBe/src/app/exercices/forms/forms-exercice3/forms-exercice3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-forms-exercice3',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsExercice3Component implements OnInit {
 
+  formNewPerson: FormGroup = new FormGroup({
+    pseudo: new FormControl('', Validators.required),
+    mdp: new FormControl('', [Validators.required, Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$')]),
+    mail: new FormControl('', [Validators.required,
+      //Validators.pattern('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])'),
+      Validators.email]),
+    date: new FormControl('', Validators.required),
+    autre: new FormGroup( {
+      poursuivre: new FormControl('', Validators.required),
+      connaissance: new FormControl('', Validators.required),
+      note: new FormControl('', Validators.required)
+    })
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  isPasswordInvalid(): boolean {
+    return this.formNewPerson.controls.mdp.invalid && this.formNewPerson.controls.mdp.touched && this.formNewPerson.controls.mdp.dirty;
+  }
 }
